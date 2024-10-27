e5b3bc63ad843c043bbff8b40c14c8e3da017de7
rashmisridasari <rashmisridasari2005@gmail.com>
2024-10-27 14:15:45 +0530

    day 6 fn

--- React/sample-project2/src/components/Navbar.jsx
+++ React/sample-project2/src/components/Navbar.jsx
@@ -1,19 +1,29 @@
-import React,{useState} from 'react'
+import React,{ useEffect, useState} from 'react'
 
 const Navbar = ({appdata,logo}) => {
     const [numberstate, setNumberstate]= useState(0)
-    const [stringstate, setStringstate]= useState("rashmi model")
+    const [stringstate, setStringstate]= useState("name")
     const [arraystate, setArraystate]= useState([
         "Hey",false,0
     ])
-    console.log(numberstate)
-    console.log(stringstate)
-    console.log(arraystate)
+    // console.log(numberstate)
+    // console.log(stringstate)
+    // console.log(arraystate)
+
+    const [count,setCount] = useState(0);
+    useEffect( () => {
+      //alert('effect rendered')
+      console.log("Count is " +count)
+    },[count])
   return (
     <>
-      {appdata.appname} {logo}
+      { /*{appdata.appname} {logo}*/ }
+      <button button className='w-[6rem] h-[4rem] bg-blue-500 text-white'
+      onClick= { () => { setCount(count+1)}} >
+        Count {count}
+    </button>
     </>
   )
 }
 
-export default Navbar
+export default Navbar 
